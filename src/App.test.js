e0a8cdaf583b449ad5648fe4271729b0945873b4
chicken-zone <zone.82440018@gmail.com>
2023-06-27 07:25:33 +0900

    feat:firstcommit

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// テストする内容を（）内に記述
+// renderはコンポーネントを指定
+// 指定コンポーネントのaタグを指定している為、LinkElementの変数にgetBytextを入れている
+// getByText(/learn react/i)のiは小文字大文字を無視する
+// expect=期待する等の意味
+// expect(linkElement).toBeInTheDocument();　リンクエレメントの中にドキュメントは存在しているかという処理
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
